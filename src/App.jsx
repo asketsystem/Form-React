0b3efaee8bb3351ab7ddd346f7de8b9734d3fdf5
asketsystem <asketsystem1@gmail.com>
2021-12-26 11:13:21 +0000

    How to prevent re-rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import "./app.css"
-import React from 'react'
+import React, {useRef} from 'react'
 import { useState } from "react/cjs/react.production.min";
+import FormInput from "./components/FormInput";
 
 const App = () => {
-    const [username,setUsername] = useState("")
+    // const [username,setUsername] = useState("")
 
-    console.log(username);
-    return <div className="app">
-        <form>
-            <FormInput placeholder="Username" setUsername={setUsername}/>
-            <FormInput placeholder="Email"/>
-            <FormInput placeholder="Full Name"/>
-            <FormInput placeholder="Sth else"/>
-        </form>
-    </div>;
-    
+    console.log("re-rendered")
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        const data = new FormData(e.target)
+        console.log(Object.fromEntries(data.entries()))
+    };
+    return (
+        <div className="app">
+            <form onSubmit={handleSubmit}>
+                <FormInput name="username" placeholder="Username" />
+                <FormInput name="email" placeholder="Email"/>
+                <FormInput name="fullname" placeholder="Full Name"/>
+                <FormInput name="sth" placeholder="Sth else"/>
+                <button>Submit</button>
+            </form>
+        </div>
+    );
 };
 
 export default App;
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -5,10 +5,7 @@ const FormInput = (props) => {
     return (
         <div className='formInput'>
             {/* <label>Username</label> */}
-            <input 
-                placeholder={props.placeholder}
-                onChange={(e) => props.setUsername(e.target.value)} 
-            />
+            <input name={props.name} placeholder={props.placeholder} />
         </div>
     )
 }
